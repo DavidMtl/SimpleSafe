e9631f9128602a10483023f0b378ef0c2b1a6e11
DavidMtl <davidmtl@protonmail.com>
2016-11-26 19:50:07 -0500

    Adding code for safe_js

--- simple_safe.js
+++ simple_safe.js
@@ -1,148 +1,77 @@
-var token = localStorage["token"];
+var token;
 // Wait for the page to be fully loaded
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("auth_button").addEventListener("click", function(){
-		authorize();
+        let app = {
+           name: 'SimpleSafe2',
+           id: 'davidmtl.simplesafe2',
+           version: '0.0.1',
+           vendor: 'davidmtl',
+           permissions: [],
+          }
+
+        window.safeAuth.authorise(app).then(function(response) {
+            console.log(response);
+            token = response.token;
+        }).catch(function(error) {
+            console.log("ERROR:" + error);
+        });
 	});
 
     document.getElementById("nfs_post_button").addEventListener("click", function(){
         var filename = document.getElementById("filename").value;
         var content = document.getElementById("content").value;
-        post(filename, content);
+      	window.safeNFS.createFile(token, filename, content).then(function(response) {
+            console.log(response);
+        }).catch(function(error) {
+            console.log("ERROR:" + error);
+        });
 	});
 
     document.getElementById("nfs_get_button").addEventListener("click", function(){
         var filename = document.getElementById("filename").value;
-        get(filename);
+
+        window.safeNFS.getFile( token, filename ).then( response =>
+        {
+            console.log(response);
+        }).catch(function(error) {
+            console.log("ERROR:" + error);
+        });
 	});
 
     document.getElementById("nfs_delete_button").addEventListener("click", function(){
         var filename = document.getElementById("filename").value;
-        remove(filename);
+        window.safeNFS.deleteFile(token, filename).then(function(response) {
+            console.log(response);
+        }).catch(function(error) {
+            console.log("ERROR:" + error);
+        });
+	});
+
+    document.getElementById("create_dir_button").addEventListener("click", function(){
+        var directory = document.getElementById("directory").value;
+      	window.safeNFS.createDir(token, directory).then(function(response) {
+            console.log(response);
+        }).catch(function(error) {
+            console.log("ERROR:" + error);
+        });
+	});
+
+    document.getElementById("get_dir_button").addEventListener("click", function(){
+        var directory = document.getElementById("directory").value;
+      	window.safeNFS.getDir(token, directory).then(function(response) {
+            console.log(response);
+        }).catch(function(error) {
+            console.log("ERROR:" + error);
+        });
+	});
+
+    document.getElementById("delete_dir_button").addEventListener("click", function(){
+        var directory = document.getElementById("directory").value;
+      	window.safeNFS.deleteDir(token, directory).then(function(response) {
+            console.log(response);
+        }).catch(function(error) {
+            console.log("ERROR:" + error);
+        });
 	});
 }, false);
-
-
-function authorize() {
-    console.log("authorizing...");
-
-    // Prepare the request
-    var request = new XMLHttpRequest();
-    request.open("post", "http://api.safenet/auth", true);
-    request.setRequestHeader("Content-type", "application/json");
-
-    // Create callback functions to handle the response.
-    request.onerror = function () {
-        console.log(this.responseText);
-    };
-    request.onload = function() {
-        if (this.status == 200) {
-            console.log("SUCCESS:" + this.responseText);
-            var response = JSON.parse(this.responseText);
-            token = response.token;
-            localStorage["token"] = token;
-        } else {
-            console.log("ERROR:" + this.responseText);
-        }
-    };
-
-    // Create the payload that will be sent with the request
-    var payload = {
-        app: {
-            name: "Simple SAFE API example",
-            version: "1.0.0",
-            vendor:  "your_name",
-            id: "your_app_id"
-        },
-        permissions: ["SAFE_DRIVE_ACCESS"]
-    };
-
-    // Send the request
-    var payload_string = JSON.stringify(payload);
-    request.send(payload_string);
-}
-
-function post(filename, content) {
-    console.log("post file:" + filename);
-
-    // Prepare the request
-    var request = new XMLHttpRequest();
-    request.open("post", "http://api.safenet/nfs/file/app/" + filename, true);
-    request.setRequestHeader("Content-type", "application/json");
-    request.setRequestHeader('Authorization', 'Bearer ' + token);
-
-    // Create callback functions to handle the response.
-    request.onerror = function () {
-        console.log(this.responseText);
-    };
-    request.onload = function() {
-        if (this.status == 200) {
-            console.log("SUCCESS:" + this.responseText);
-        } else {
-            console.log("ERROR:" + this.responseText);
-        }
-    };
-
-    // Create the payload that will be sent with the request
-    var payload = {
-        content:content
-    };
-
-    // Send the request
-    var payload_string = JSON.stringify(payload);
-    request.send(payload_string);
-}
-
-function get(filename) {
-    console.log("get file:" + filename);
-
-    // Prepare the request
-    var request = new XMLHttpRequest();
-    request.open("get", "http://api.safenet/nfs/file/app/" + filename, true);
-    request.setRequestHeader("Content-type", "application/json");
-    request.setRequestHeader('Authorization', 'Bearer ' + token);
-
-    // Create callback functions to handle the response.
-    request.onerror = function () {
-        console.log(this.responseText);
-    };
-    request.onload = function() {
-        if (this.status == 200) {
-            console.log("SUCCESS:" + this.responseText);
-        } else {
-            console.log("ERROR:" + this.responseText);
-        }
-    };
-
-    // There's no payload for the get request
-
-    // Send the request
-    request.send();
-}
-
-function remove(filename) {
-    console.log("delete file:" + filename);
-
-    // Prepare the request
-    var request = new XMLHttpRequest();
-    request.open("delete", "http://api.safenet/nfs/file/app/" + filename, true);
-    request.setRequestHeader("Content-type", "application/json");
-    request.setRequestHeader('Authorization', 'Bearer ' + token);
-
-    // Create callback functions to handle the response.
-    request.onerror = function () {
-        console.log(this.responseText);
-    };
-    request.onload = function() {
-        if (this.status == 200) {
-            console.log("SUCCESS:" + this.responseText);
-        } else {
-            console.log("ERROR:" + this.responseText);
-        }
-    };
-
-    // There's no payload for the delete request
-
-    // Send the request
-    request.send();
-}
